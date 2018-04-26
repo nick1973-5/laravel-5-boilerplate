67ac045fb23bc996ccf5b1466da11b50270fbbc1
Nick Ashford <N.Ashford@Internal.growthpartnersplc.co.uk>
2018-04-26 14:40:15 +0100

    First draft of home page

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a href="{{ route('frontend.index') }}" class="navbar-brand"><img src="/img/frontend/iben_logo.png"></a>
 
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('labels.general.toggle_navigation') }}">
@@ -19,44 +19,55 @@
             @auth
                 <li class="nav-item">
                     <a href="{{route('frontend.user.dashboard')}}"
-                       class="nav-link {{ active_class(Active::checkRoute('frontend.user.dashboard')) }}">
-                        <span class="fas fa-user"></span> Account
+                       class="nav-link {{ active_class(Active::checkRoute('frontend.user.dashboard')) }} text-danger">
+                        <span class="fas fa-user text-danger"></span> Account
                     </a>
                 </li>
             @endauth
 
             @guest
-                <li class="nav-item"><a href="{{route('frontend.auth.login')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.auth.login')) }}">{{ __('navs.frontend.login') }}</a></li>
-
+                <li class="nav-item">
+                    <a href="{{route('frontend.auth.login')}}"
+                       class="text-danger text-danger nav-link {{ active_class(Active::checkRoute('frontend.auth.login')) }}">
+                        {{ __('navs.frontend.login') }}
+                    </a>
+                </li>
                 @if (config('access.registration'))
                     <li class="nav-item">
-                        <a href="{{route('frontend.auth.register')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.auth.register')) }}">
-                            {{ __('navs.frontend.register') }}</a>
+                        <a href="{{route('frontend.auth.register')}}"
+                           class="text-danger nav-link {{ active_class(Active::checkRoute('frontend.auth.register')) }}">
+                            {{ __('navs.frontend.register') }}
+                        </a>
                     </li>
                 @endif
             @else
 
                 <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuUser" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false">{{ $logged_in_user->name }}</a>
-
+                    <a href="#" class="text-danger nav-link dropdown-toggle" id="navbarDropdownMenuUser" data-toggle="dropdown"
+                       aria-haspopup="true" aria-expanded="false">{{ $logged_in_user->name }}
+                    </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuUser">
                         @can('view backend')
-                            <a href="{{ route('admin.dashboard') }}" class="dropdown-item">{{ __('navs.frontend.user.administration') }}</a>
+                            <a href="{{ route('admin.dashboard') }}"
+                               class="text-danger dropdown-item">{{ __('navs.frontend.user.administration') }}
+                            </a>
                         @endcan
 
-                        <a href="{{ route('frontend.user.account') }}" class="dropdown-item {{ active_class(Active::checkRoute('frontend.user.account')) }}">
+                        <a href="{{ route('frontend.user.account') }}"
+                           class="text-danger dropdown-item {{ active_class(Active::checkRoute('frontend.user.account')) }}">
                             Your Profile
                         </a>
-                        <a href="{{ route('frontend.auth.logout') }}" class="dropdown-item">{{ __('navs.general.logout') }}</a>
+                        <a href="{{ route('frontend.auth.logout') }}"
+                           class="text-danger dropdown-item">{{ __('navs.general.logout') }}
+                        </a>
                     </div>
                 </li>
             @endguest
 
             {{--<li class="nav-item"><a href="{{route('frontend.contact')}}" class="nav-link {{ active_class(Active::checkRoute('frontend.contact')) }}">{{ __('navs.frontend.contact') }}</a></li>--}}
             <li class="nav-item">
-                <a href="#" class="nav-link {{ active_class(Active::checkRoute('frontend.contact')) }}">
-                    <span class="fas fa-balance-scale"></span> Balance (£1,000)
+                <a href="#" class="text-danger nav-link {{ active_class(Active::checkRoute('frontend.contact')) }}">
+                    <span class="text-danger fas fa-balance-scale"></span> Balance (£1,000)
                 </a>
             </li>
         </ul>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -37,51 +37,61 @@
         <span class="sr-only">Next</span>
     </a>
 
-    <div class="container">
+    <div class="container mt-4">
         <div class="row">
+            <div class="col-md-4">
+                <a style="text-decoration:none" class=" " href="#">
+                    <div class="card mb-4 box-shadow bg-dark text-light text-center">
+                        <div class="card-body">
+                            <p class="card-text">SHOP</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a style="text-decoration:none" href="#">
+                    <div class="card mb-4 box-shadow bg-dark text-light text-center">
+                        <div class="card-body">
+                            <p class="card-text">DIRECTORY</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a style="text-decoration:none" class=" " href="#">
+                    <div class="card mb-4 box-shadow bg-dark text-light text-center">
+                        <div class="card-body">
+                            <p class="card-text">CASH OUT</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
             <div class="row">
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" src="img/frontend/shopfittings_small.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                </div>
-                                <small class="text-muted">9 mins</small>
-                            </div>
+                        <div class="card-body bg-secondary">
+                            <p class="card-text text-center text-light">Shop Fitting</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" src="img/frontend/equipment.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                </div>
-                                <small class="text-muted">9 mins</small>
-                            </div>
+                        <div class="card-body bg-secondary">
+                            <p class="card-text text-center text-light">Equipment</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" src="img/frontend/medical.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                </div>
-                                <small class="text-muted">9 mins</small>
-                            </div>
+                        <div class="card-body bg-secondary">
+                            <p class="card-text text-center text-light">Medical Insurance</p>
                         </div>
                     </div>
                 </div>
@@ -89,53 +99,66 @@
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" src="img/frontend/banner.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                </div>
-                                <small class="text-muted">9 mins</small>
-                            </div>
+                        <div class="card-body bg-secondary">
+                            <p class="card-text text-center text-light">Team Building</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" src="img/frontend/legal.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                </div>
-                                <small class="text-muted">9 mins</small>
-                            </div>
+                        <div class="card-body bg-secondary">
+                            <p class="card-text text-center text-light">Legal Services</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
                         <img class="card-img-top" src="img/frontend/insurance.jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                </div>
-                                <small class="text-muted">9 mins</small>
-                            </div>
+                        <div class="card-body bg-secondary">
+                            <p class="card-text text-center text-light">Insurance Services</p>
                         </div>
                     </div>
                 </div>
-
             </div>
-        </div><!-- row -->
     </div><!-- container -->
-    <div>
-        <p>This is the footer</p>
+    <div class="container">
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <img class="mb-2" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+                    <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Features</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Cool stuff</a></li>
+                        <li><a class="text-muted" href="#">Random feature</a></li>
+                        <li><a class="text-muted" href="#">Team feature</a></li>
+                        <li><a class="text-muted" href="#">Stuff for developers</a></li>
+                        <li><a class="text-muted" href="#">Another one</a></li>
+                        <li><a class="text-muted" href="#">Last time</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Resources</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Resource</a></li>
+                        <li><a class="text-muted" href="#">Resource name</a></li>
+                        <li><a class="text-muted" href="#">Another resource</a></li>
+                        <li><a class="text-muted" href="#">Final resource</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>About</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Team</a></li>
+                        <li><a class="text-muted" href="#">Locations</a></li>
+                        <li><a class="text-muted" href="#">Privacy</a></li>
+                        <li><a class="text-muted" href="#">Terms</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     </div>
 @endsection
